28d91c4783e4e8ed7fe3745c395699ad7066916c
Lucas Katayama <lucas.katayama@allin.com.br>
2022-12-12 11:32:32 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct URL

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest',
+    '.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
-    '\\.scss$': 'identity-obj-proxy',
   },
 };
