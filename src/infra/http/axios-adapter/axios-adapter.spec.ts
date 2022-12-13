f2ffdf41398f41e6d3556005229223a5d52c83f8
Lucas Katayama <lucas.katayama@allin.com.br>
2022-12-12 21:57:34 -0300

    refactor: move psot request creation to a factory method

--- src/infra/http/axios-adapter/axios-adapter.spec.ts
+++ src/infra/http/axios-adapter/axios-adapter.spec.ts
@@ -1,6 +1,7 @@
 import { AxiosAdapter } from './axios-adapter'
 import axios from 'axios'
 import faker from 'faker'
+import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -9,11 +10,16 @@ const makeSut = (): AxiosAdapter => {
   return new AxiosAdapter()
 }
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement()
+})
+
 describe('AxiosAdapter', () => {
   test('Should call axios with correct URL and verb', async () => {
-    const url = faker.internet.url()
+    const request = mockPostRequest()
     const sut = makeSut()
-    sut.post({ url })
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+    sut.post(request)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
   })
 })
