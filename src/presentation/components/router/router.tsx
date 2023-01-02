074e45a31df15574a42dac5fa1d1b9b31d44192b
Lucas Barbosa Katayama <lucaskatayama12@gmail.com>
2023-01-02 12:22:27 -0300

    feat: ensure Login calls validation with correct email

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,11 +1,10 @@
-import { Login } from '@/presentation/pages'
 import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/login',
-    element: <Login />
+    element: <></>
   }
 ])
 
