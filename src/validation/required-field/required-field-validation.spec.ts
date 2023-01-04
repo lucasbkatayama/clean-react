43f11d8a1c4ae2f114ba7ff63cc937b55912d6dc
Lucas Barbosa Katayama <lucaskatayama12@gmail.com>
2023-01-04 14:52:17 -0300

    test: ensure RequiredFieldValidation returns null if field is valid

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
 import { RequiredFieldError } from '@/validation/error'
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
   it('should return error if field is empty', () => {
     const sut = new RequiredFieldValidation('any_field')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  it('should return null if field is valid', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeNull()
   })
 })
