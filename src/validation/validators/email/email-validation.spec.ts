9fe35d25791fa64fea04d57a1a26b11382ee9782
Lucas Barbosa Katayama <lucaskatayama12@gmail.com>
2023-01-04 15:24:59 -0300

    test: ensure EmailValidation returns null if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFieldError } from '@/validation/error'
 import { EmailValidation } from '@/validation/validators/email/email-validation'
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   it('should return error if email is invalid', () => {
-    const sut = new EmailValidation('any_field')
+    const sut = new EmailValidation(faker.random.word())
     const error = sut.validate('not-valid-value')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  it('should return null if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeNull()
   })
 })
