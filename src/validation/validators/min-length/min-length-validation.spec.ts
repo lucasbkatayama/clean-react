46e435b44dee426eb80daabdbf82d826303b8199
Lucas Barbosa Katayama <lucaskatayama12@gmail.com>
2023-01-04 15:52:11 -0300

    test: ensure MinLengthValidation return null if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,12 +2,18 @@ import { InvalidFieldError } from '@/validation/error'
 import faker from 'faker'
 import { MinLengthValidation } from './min-length-validation'
 
-const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.random.word())
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.random.word(), 5)
 
 describe('MinLengthValidation', () => {
   it('should return error if value is invalid', () => {
     const sut = makeSut()
-    const error = sut.validate('123')
+    const error = sut.validate(faker.random.alphaNumeric(3))
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  it('should return null if value is valid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(5))
+    expect(error).toBeNull()
   })
 })
