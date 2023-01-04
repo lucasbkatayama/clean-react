7097752063a9690010fa5c74ac4636ed3cbd061b
Lucas Barbosa Katayama <lucaskatayama12@gmail.com>
2023-01-04 19:34:27 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,13 +4,14 @@ import { ValidationComposite } from './validation-composite'
 describe('ValidationComposite', () => {
   it('should return error if any validation fail', () => {
     const validationSpy = new ValidationSpy('any_field')
+    validationSpy.error = new Error('first_error_message')
     const validationSpy2 = new ValidationSpy('any_field')
-    validationSpy2.error = new Error('any_error_message')
+    validationSpy2.error = new Error('second_error_message')
     const sut = new ValidationComposite([
       validationSpy,
       validationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
   })
 })
