a8d5756d085ef995e8887c5b219254e2ea55405d
Lucas Barbosa Katayama <lucaskatayama12@gmail.com>
2023-01-05 14:39:17 -0300

    test: ensure ValidationBuilder return a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,18 +1,34 @@
 import { EmailValidation, MinLengthValidation, RequiredFieldValidation, ValidationBuilder } from '@/validation/validators'
+import faker from 'faker'
 
 describe('ValidationBuilder', () => {
   it('should return RequiredFieldValidation', () => {
-    const requiredFieldValidation = ValidationBuilder.field('any_field').required().build()
-    expect(requiredFieldValidation).toEqual([new RequiredFieldValidation('any_field')])
+    const field = faker.database.column()
+    const requiredFieldValidation = ValidationBuilder.field(field).required().build()
+    expect(requiredFieldValidation).toEqual([new RequiredFieldValidation(field)])
   })
 
   it('should return EmailValidation', () => {
-    const requiredFieldValidation = ValidationBuilder.field('any_field').email().build()
-    expect(requiredFieldValidation).toEqual([new EmailValidation('any_field')])
+    const field = faker.database.column()
+    const requiredFieldValidation = ValidationBuilder.field(field).email().build()
+    expect(requiredFieldValidation).toEqual([new EmailValidation(field)])
   })
 
   it('should return MinLengthValidation', () => {
-    const requiredFieldValidation = ValidationBuilder.field('any_field').min(5).build()
-    expect(requiredFieldValidation).toEqual([new MinLengthValidation('any_field', 5)])
+    const field = faker.database.column()
+    const length = faker.datatype.number()
+    const requiredFieldValidation = ValidationBuilder.field(field).min(length).build()
+    expect(requiredFieldValidation).toEqual([new MinLengthValidation(field, length)])
+  })
+
+  it('should return a list of Validations', () => {
+    const field = faker.database.column()
+    const length = faker.datatype.number()
+    const requiredFieldValidation = ValidationBuilder.field(field).min(length).required().email().build()
+    expect(requiredFieldValidation).toEqual([
+      new MinLengthValidation(field, length),
+      new RequiredFieldValidation(field),
+      new EmailValidation(field)
+    ])
   })
 })
